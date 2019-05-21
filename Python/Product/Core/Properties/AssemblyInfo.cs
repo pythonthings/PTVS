9af3362047782426a2f7a6f1f53ccbb91982c3e7
Hugues Valois <huvalo@microsoft.com>
2019-05-21 12:46:08 -0700

    Stop using ProvideCodeBaseAttribute, which fails to load the assembly when building on microbuild machine.

--- Python/Product/Core/Properties/AssemblyInfo.cs
+++ Python/Product/Core/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
+using Microsoft.PythonTools.Core;
 using Microsoft.VisualStudio.Shell;
 
 [assembly: AssemblyTitle("Visual Studio - Python support")]
@@ -27,11 +28,37 @@ using Microsoft.VisualStudio.Shell;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Analysis", CodeBase = @"LanguageServer\x86\Microsoft.Python.Analysis.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Analysis.Core", CodeBase = @"LanguageServer\x86\Microsoft.Python.Analysis.Core.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Core", CodeBase = @"LanguageServer\x86\Microsoft.Python.Core.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.LanguageServer", CodeBase = @"LanguageServer\x86\Microsoft.Python.LanguageServer.dll")]
-[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Parsing", CodeBase = @"LanguageServer\x86\Microsoft.Python.Parsing.dll")]
+[assembly: ProvideRawCodeBase(
+    AssemblyName = "Microsoft.Python.Analysis", CodeBase = @"$PackageFolder$\LanguageServer\x86\Microsoft.Python.Analysis.dll",
+    Version = LSConstants.Version, Culture = LSConstants.Culture, PublicKeyToken = LSConstants.PublicKeyToken
+)]
+
+[assembly: ProvideRawCodeBase(
+    AssemblyName = "Microsoft.Python.Analysis.Core", CodeBase = @"$PackageFolder$\LanguageServer\x86\Microsoft.Python.Analysis.Core.dll",
+    Version = LSConstants.Version, Culture = LSConstants.Culture, PublicKeyToken = LSConstants.PublicKeyToken
+)]
+
+[assembly: ProvideRawCodeBase(
+    AssemblyName = "Microsoft.Python.Core", CodeBase = @"$PackageFolder$\LanguageServer\x86\Microsoft.Python.Core.dll",
+    Version = LSConstants.Version, Culture = LSConstants.Culture, PublicKeyToken = LSConstants.PublicKeyToken
+)]
+
+[assembly: ProvideRawCodeBase(
+    AssemblyName = "Microsoft.Python.LanguageServer", CodeBase = @"$PackageFolder$\LanguageServer\x86\Microsoft.Python.LanguageServer.dll",
+    Version = LSConstants.Version, Culture = LSConstants.Culture, PublicKeyToken = LSConstants.PublicKeyToken
+)]
+
+[assembly: ProvideRawCodeBase(
+    AssemblyName = "Microsoft.Python.Parsing", CodeBase = @"$PackageFolder$\LanguageServer\x86\Microsoft.Python.Parsing.dll",
+    Version = LSConstants.Version, Culture = LSConstants.Culture, PublicKeyToken = LSConstants.PublicKeyToken
+)]
+
+// LSC - can't use these on build machine for unknown reason (it fails to load the assembly when registering)
+//[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Analysis", CodeBase = @"LanguageServer\x86\Microsoft.Python.Analysis.dll")]
+//[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Analysis.Core", CodeBase = @"LanguageServer\x86\Microsoft.Python.Analysis.Core.dll")]
+//[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Core", CodeBase = @"LanguageServer\x86\Microsoft.Python.Core.dll")]
+//[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.LanguageServer", CodeBase = @"LanguageServer\x86\Microsoft.Python.LanguageServer.dll")]
+//[assembly: ProvideCodeBase(AssemblyName = "Microsoft.Python.Parsing", CodeBase = @"LanguageServer\x86\Microsoft.Python.Parsing.dll")]
 
 [assembly: ProvideCodeBase(AssemblyName = "Microsoft.PythonTools", CodeBase = "Microsoft.PythonTools.dll", Version = AssemblyVersionInfo.StableVersion)]
 [assembly: ProvideCodeBase(AssemblyName = "Microsoft.PythonTools.Attacher", CodeBase = "Microsoft.PythonTools.Attacher.exe", Version = AssemblyVersionInfo.StableVersion)]
@@ -45,3 +72,10 @@ using Microsoft.VisualStudio.Shell;
 [assembly: ProvideCodeBase(AssemblyName = "Microsoft.PythonTools.VSCommon", CodeBase = "Microsoft.PythonTools.VSCommon.dll", Version = AssemblyVersionInfo.StableVersion)]
 [assembly: ProvideCodeBase(AssemblyName = "Microsoft.PythonTools.VSInterpreters", CodeBase = "Microsoft.PythonTools.VSInterpreters.dll", Version = AssemblyVersionInfo.StableVersion)]
 [assembly: ProvideCodeBase(AssemblyName = "Microsoft.PythonTools.Workspace", CodeBase = "Microsoft.PythonTools.Workspace.dll", Version = AssemblyVersionInfo.StableVersion)]
+
+internal static class LSConstants {
+    // important: keep in sync with Build\PreBuild.ps1
+    public const string Version = "0.2.82.0";
+    public const string Culture = "neutral";
+    public const string PublicKeyToken = "b03f5f7f11d50a3a";
+}
